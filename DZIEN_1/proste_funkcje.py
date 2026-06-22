f6d620f32a4e586ec21a9e902a363ec2278bbb85
albim72 <marcin.albiniak72@gmail.com>
2026-06-22 15:21:05 +0200

    Update proste_funkcje.py

--- DZIEN_1/proste_funkcje.py
+++ DZIEN_1/proste_funkcje.py
@@ -37,8 +37,17 @@ def policz_rabat(kwota:float, stala:float, rabat:float= 0.12)->float:
 print(policz_rabat(2500,12,0.17))
 print(policz_rabat(2900,100,0.12))
 print(policz_rabat(2500,88))
+print("fukcja z wieloma wartoścami domyślnymi")
+def oblicz_f(n:float,m:float,k:int=9,z:float=3.3)->float:
+    return 2*n+k*m+z**2
 
-
+print(oblicz_f(2,3,4))
+print(oblicz_f(24,2,4,8))
+print(oblicz_f(-2,3))
+print(oblicz_f(56,86))
+print(oblicz_f(7.33,-3.45,z=7))
+print(oblicz_f(7.33,-3.45,k=4,z=7))
+print(oblicz_f(m=7.8,k=1.34,n=9,z=0.44))
 
 
 
