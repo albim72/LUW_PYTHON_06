52d826a0f79f6806ed8226131ea93c03dcfadd65
albim72 <marcin.albiniak72@gmail.com>
2026-06-22 10:29:37 +0200

    Add introduction script for variable types and IDs

---
+++ DZIEN_1/wprowadzenie.py
@@ -0,0 +1,25 @@
+a = 5
+print(a)
+print(type(a))
+
+b = a
+print(b)
+print(type(b))
+
+c = 5
+print(c)
+print(type(c))
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+a = 10
+print(a)
+print(type(a))
+print(id(a))
+
+a = "dzień dobry"
+print(a)
+print(type(a))
+print(id(a))
