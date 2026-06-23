cf68269e952a056bec61e47b10a7854dd10e90a0
albim72 <marcin.albiniak72@gmail.com>
2026-06-23 09:27:55 +0200

    Create gross_function.py

---
+++ DZIEN_2/gross_function.py
@@ -0,0 +1,34 @@
+#fukcja calc_gross_price() - licząca wartość brutto produktu
+
+def calc_gross_price(net_price:float,vat_percent:float)->float:
+    gross_price = net_price + net_price * vat_percent/100
+    return gross_price
+
+"""
+program oparty na funkcji fukcja calc_gross_price()
+może wykonać dowolene obliczenie dla każdego produktu z dowlony m VAT
+parametry:
+net_price:float,
+vat_percent:float
+"""
+
+price = calc_gross_price(121,23)
+print(f"cena brutto wynosi: {price:.2f} zł")
+
+opis = """
+program oparty na funkcji fukcja calc_gross_price()
+może wykonać dowolene obliczenie dla każdego produktu z dowlony m VAT
+parametry:
+net_price:float,
+vat_percent:float
+"""
+
+print(opis)
+
+#ceny wielu produktów
+prices = [22.6,304,101.99,20,89,3.4,1023.77,202,90]
+
+"""
+zadanie uzupełniające - użyj fukcji calc_gross_price() do policzenia cen brutto dla
+wszystkich cen netto produktów zawartych w liście prices 
+"""
