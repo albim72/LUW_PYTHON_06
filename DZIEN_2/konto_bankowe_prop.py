385ccf8428c7694b78a1e1477405bd7580d0cb5f
albim72 <marcin.albiniak72@gmail.com>
2026-06-23 14:59:11 +0200

    Update konto_bankowe_prop.py

--- DZIEN_2/konto_bankowe_prop.py
+++ DZIEN_2/konto_bankowe_prop.py
@@ -2,25 +2,25 @@ class KontoBankowe:
     def __init__(self,wlasciciel:str, saldo:float=0.0):
         self._wlasciciel = wlasciciel
         self._saldo = saldo
-        
+
     @property
     def wlasciciel(self):
         return self._wlasciciel
-    
+
     @wlasciciel.setter
     def wlasciciel(self,wlasciciel):
         self._wlasciciel = wlasciciel
-        
+
     @property
     def saldo(self):
         return self._saldo
-    
+
     @saldo.setter
     def saldo(self,kwota:float):
         if kwota < 0:
             raise ValueError("kwota nie może być ujemna!")
         self._saldo = kwota
-        
+
     @saldo.deleter
     def saldo(self):
         print("saldo zostało wyzerowane....")
@@ -30,3 +30,17 @@ class KontoBankowe:
 if __name__ == '__main__':
     konto = KontoBankowe('Jakub')
     print(konto)
+
+    print(konto.wlasciciel)
+    print(konto.saldo)
+
+    konto.saldo = 2360
+    print(konto.saldo)
+
+    del konto.saldo
+    print(konto.saldo)
+
+    try:
+        konto.saldo = -1000
+    except ValueError as e:
+        print(e)
