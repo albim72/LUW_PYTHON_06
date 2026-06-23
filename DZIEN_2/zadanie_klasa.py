1f7cb1fde8a6e809436de217af41b401885951df
albim72 <marcin.albiniak72@gmail.com>
2026-06-23 14:17:08 +0200

    Update zadanie_klasa.py

--- DZIEN_2/zadanie_klasa.py
+++ DZIEN_2/zadanie_klasa.py
@@ -11,8 +11,47 @@ class Car:
         print(f"Paliwo: {self.fuel} litrów")
         print(f"Przebieg: {self.mileage} km")
 
+    def refuel(self, amount):
+        if amount <= 0:
+            print("Ilość paliwa musi być większa od zera.")
+        else:
+            self.fuel += amount
+            print(f"Zatankowano {amount} litrów paliwa.")
+
+    def drive(self, distance):
+        if distance <= 0:
+            print("Dystans musi być większy od zera.")
+            return
+
+        needed_fuel = distance / 10
+
+        if needed_fuel > self.fuel:
+            print("Za mało paliwa na tę trasę.")
+        else:
+            self.fuel -= needed_fuel
+            self.mileage += distance
+            print(f"Przejechano {distance} km.")
+
+    def is_old(self):
+        return self.mileage > 200000
+
+
 if __name__ == '__main__':
 
     car = Car("Toyota", "Corolla", 20, 150000)
 
+    print("DANE POCZĄTKOWE:")
     car.show_info()
+
+    print()
+    car.refuel(10)
+
+    print()
+    car.drive(50)
+
+    print()
+    print("DANE PO TANKOWANIU I JEŹDZIE:")
+    car.show_info()
+
+    print()
+    print("Czy samochód jest stary?", car.is_old())
