f3706b911729d006d08d406cd19d37e965fdc41a
albim72 <marcin.albiniak72@gmail.com>
2026-06-23 13:25:03 +0200

    Update bookstore.py

--- DZIEN_2/bookstore.py
+++ DZIEN_2/bookstore.py
@@ -11,13 +11,42 @@ class Book:
         self.bookstore_nb = bookstore_nb
         self.pages = pages
         self.binding = "miękka"
+        self.newbook()
+
+    def __repr__(self):
+        return f"{self.title} by {self.author}, binding: {self.binding}, price: {self.price}, bookstore_nb: {self.bookstore_nb}"
     #zachowanie
+
+    def newbook(self):
+        print("Nowy obiekt klasy Book!")
+
+    def get_binding(self):
+        return self.binding
+
+    def set_binding(self,binding):
+        self.binding = binding
+
+    def cena_rabat(self,procent):
+        self.price = self.price - self.price * procent/100
+        return self.price
+
+    def obwoluta(self):
+        return self.__kolor
+
 
 if __name__ == '__main__':
     b1 = Book("ABC ultra biegacza","Marcin Świerc",102,345,22)
     print(b1)
-    print(b1.binding)
+    print(b1.binding) # zła praktyka
     print(b1._grubosc)
     # print(b1.__kolor)
+    b1.price = 99 # bardzo zła praktyka
+    print(b1)
+    print(b1.__class__.__name__)
 
+    print(b1.get_binding())
+    b1.set_binding("twarda")
+    print(b1.get_binding())
+    print(f"cena po rabacie 6% : {b1.cena_rabat(6)} zł")
+    print(f"kolor obowluty: {b1.obwoluta()}")
 
