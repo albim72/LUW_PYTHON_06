bd7382690b051613a7d99b81437d22955d9a3e5d
albim72 <marcin.albiniak72@gmail.com>
2026-06-23 15:16:51 +0200

    Update main.py

--- DZIEN_2/MOJEMODULY/main.py
+++ DZIEN_2/MOJEMODULY/main.py
@@ -1,1 +1,12 @@
+# import opis as op
+from opis import informacja,policz
+from kalk import Kalkulator
 
+print(informacja("algorytm ewolucyjny dla python"))
+print(policz(4,6,32))
+
+print("_"*70)
+k = Kalkulator(11,8,56,3,18)
+print(k.wartA())
+print("_"*70)
+print(k.wartB())
