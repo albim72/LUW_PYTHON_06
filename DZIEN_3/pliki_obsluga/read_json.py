26b8f1d8dad4ac57a17499bffb8e3317f3966801
albim72 <marcin.albiniak72@gmail.com>
2026-06-25 09:52:06 +0200

    Update read_json.py

--- DZIEN_3/pliki_obsluga/read_json.py
+++ DZIEN_3/pliki_obsluga/read_json.py
@@ -11,17 +11,36 @@ try:
 
     #normalizacja
     if isinstance(data, dict):
-        student = [data]
+        students = [data]
     elif isinstance(data, list):
-        student = data
+        students = data
     else:
         raise ValueError("Niepoprawna struktura JSON - oczekiwano obiektu lub listy")
 
-    print(student)
-    print(student[0]["city"])
+    print(students)
+    print(students[0]["city"])
 
     print("_" * 70)
 
+    print("lISTA STUDENTÓW\n")
+    for i,s in enumerate(students,start=1):
+        if not isinstance(s, dict):
+            raise ValueError(f"Element {i} nie jest obiektem JSON")
+        required_keys = ["name", "surname", "points"]
+        missing = [key for key in required_keys if key not in s]
+        if missing:
+            raise ValueError(f"Brakuje pól {missing} w elemencie #{i}")
+
+        name = s["name"]
+        surname = s["surname"]
+        points = s["points"]
+        print(f"Student: {name}, {surname} -> {points}")
 
 except IOError as e:
+    print(e)
+except ValueError as e:
+    print(e)
+except FileNotFoundError as e:
+    print(e)
+except json.decoder.JSONDecodeError as e:
     print(e)
